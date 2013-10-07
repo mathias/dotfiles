bf3a6f2eb5097e0248a5e8450ce4b40a71d536c3
Matt Gauger <matt.gauger@gmail.com>
2013-10-07 11:26:50 -0500

    Add emacs-live add user-pack
    - Using emacs-live for emacs config now
    - mv .emacs config out of the way

---
+++ emacs/mathiasx-pack/config/bindings.el
@@ -0,0 +1,10 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; change font size, interactively
+(global-set-key (kbd "s-+") 'text-scale-increase)
+(global-set-key (kbd "s-=") 'text-scale-increase)
+(global-set-key (kbd "s--") 'text-scale-decrease)
---
+++ emacs/mathiasx-pack/init.el
@@ -0,0 +1,9 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+'(text-scale-mode-step 1.2)
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
