17ff478ef194836606dd280c92f8c08a6a24973b
Matt Gauger <matt.gauger@gmail.com>
2013-12-21 12:45:55 -0600

    Add Slime support to Emacs for Common Lisp

--- emacs/mathiasx-pack/init.el
+++ emacs/mathiasx-pack/init.el
@@ -11,6 +11,15 @@
 ;; Load MobileOrg org mode settings
 (live-load-config-file "orgmode.el")
 
+;; Slime for Common Lisp REPL:
+(setq slime-lisp-implementations
+      '((sbcl ("sbcl" "--core" "sbcl.core-for-slime"))))
+(live-add-pack-lib "slime")
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(add-to-list 'load-path "~/slime")
+(require 'slime)
+(slime-setup)
+
 ;; Slamhound for Clojure
 (live-add-pack-lib "slamhound")
 (require 'slamhound)
